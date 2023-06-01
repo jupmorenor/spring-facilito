e87376cb2e8677f6d75d5ae0d0e84b22ad7e01d0
jupmorenor <jupmorenor@gmail.com>
2023-06-01 10:06:08 -0500

    generacion de proyecto catalogo de peliculas con entidades

---
+++ src/test/java/com/juanpa/springfacilito/peliculas/PeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanpa.springfacilito.peliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
